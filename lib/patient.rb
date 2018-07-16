1a25280b65377c86dd3cf81faade1243776c228d
George  Pianka <gpianka1@gmail.com>
2018-07-16 03:21:24 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -7,11 +7,19 @@ class Patient
     @@all << self 
   end 
   
-  def 
-    
+  def new_appointment(date, doctor)
+    Appointment.new(date, doctor, self) 
+  end 
   
+  def appointments 
+    Appointments.all.select {|appointment| appointment.patient == self}
+  end 
   
+  def doctors 
+    self.appointments.collect {|appointment| appointment.doctor} 
+  end
   
   def self.all 
     @@all 
   end 
+end
